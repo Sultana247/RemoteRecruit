4c27343d53f2eb1abab4bfe7af94bcea6fc4465d
Sultana247 <jahansultana302@gmail.com>
2026-06-18 12:53:57 +0600

    completed navbar for both mobile and large device

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,17 @@
+// import { Outlet } from "react-router";
+// import Navbar from "../Navbar/Navbar";
+
+import { Outlet } from "react-router";
+import Navbar from "../Navbar/Navbar";
+
+const Main = () => {
+    return (
+        <div className="flex flex-col md:flex-row min-h-screen gap-6">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+          
+        </div>
+    );
+};
+
+export default Main;
